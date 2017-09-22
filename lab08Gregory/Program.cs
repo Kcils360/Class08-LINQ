0078b5e8379c7340c0699caeb3407c98ce484849
Gregory Dukes <kcils360@gmail.com>
2017-09-21 21:11:19 -0700

    pr for credit

---
+++ lab08Gregory/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using QuickType;
+using System.Linq;
+
+namespace lab08Gregory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "data.json";
+            string path = Path.Combine(Environment.CurrentDirectory, fileName);
+            string jsonString;
+            using (StreamReader sr = File.OpenText(path))
+            {
+                jsonString = File.ReadAllText(path);
+                //Console.WriteLine(jsonString);
+            }
+            List<string> filtered = new List<string>();
+
+            var data = ManhattanNeighborhoods.FromJson(jsonString);
+            //Console.WriteLine(data);
+
+            IEnumerable<Feature> allHoods = from n in data.Features
+                                            where n.Properties.Neighborhood != ""
+                                            select n;
+
+            foreach (var n in allHoods)
+            {
+                filtered.Add(n.Properties.Neighborhood);
+                Console.WriteLine(n.Properties.Neighborhood);
+            }
+            Console.Read();
+
+            Console.WriteLine("All Neighborhoods without Nulls");
+
+
+
+        }
+    }
+
+}
